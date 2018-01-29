8657d55acde0319255d661855cb034b5008dcef8
Xinghao Zhou <xzzhou@caltech.edu>
2018-01-28 20:43:18 -0800

    150000 iterations

--- seq2seq_translation_tutorial.py
+++ seq2seq_translation_tutorial.py
@@ -800,7 +800,7 @@ if use_cuda:
     attn_decoder1 = attn_decoder1.cuda()
 
 #trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
-trainIters(encoder1, attn_decoder1, 10000, print_every=5000)
+trainIters(encoder1, attn_decoder1, 150000, print_every=5000)
 
 ######################################################################
 #

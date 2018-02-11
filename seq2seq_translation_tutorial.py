c42f2b05cea69d684616ab3d5b0786f23392fdcc
Xinghao Zhou <xzzhou@caltech.edu>
2018-02-11 11:44:34 -0800

    use the cornel data processed by simpleseq2seq with 190000iterations

--- seq2seq_translation_tutorial.py
+++ seq2seq_translation_tutorial.py
@@ -281,7 +281,7 @@ def prepareData(lang1, lang2, reverse=False):
     return input_lang, output_lang, pairs
 
 
-input_lang, output_lang, pairs = prepareData('diag1', 'diag2-3', False)
+input_lang, output_lang, pairs = prepareData('diag1', 'diag2-4-from-other', False)
 print(random.choice(pairs))
 
 
@@ -800,7 +800,7 @@ if use_cuda:
     attn_decoder1 = attn_decoder1.cuda()
 
 #trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
-trainIters(encoder1, attn_decoder1, 1000000, print_every=5000)
+trainIters(encoder1, attn_decoder1, 190000, print_every=5000)
 
 ######################################################################
 #
@@ -865,7 +865,7 @@ evaluateAndShowAttention("you always been this selfish ?")
 
 evaluateAndShowAttention("who knows ?")
 
-evaluateAndShowAttention("Yeah , I haven't really absorbed that one .".lower())
+evaluateAndShowAttention("Yeah I haven't really absorbed that one .".lower())
 
 evaluateAndShowAttention("Fuck you !".lower())
 

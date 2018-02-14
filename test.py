bebbff08329f4eb538f6bedb6334dcbe06872d55
Xinghao Zhou <xzzhou@caltech.edu>
2018-02-14 11:16:10 -0800

    add new files generate model

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 11:34:37 2018
+
+@author: Xinghao
+"""
+
+import torch
+from model import *
+
+a = torch.load('attn_decoder1a.pth')

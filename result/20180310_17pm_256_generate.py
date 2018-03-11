f81a37684a2e08515734edca1441cc5d6cb8bcfe
Ubuntu <ubuntu@ip-172-31-26-112.us-west-2.compute.internal>
2018-03-11 04:54:40 +0000

    add new result file

---
+++ result/20180310_17pm_256_generate.py
@@ -0,0 +1,66 @@
+/home/ubuntu/xinghao/cs101-xinghao/model.py:37: UserWarning: RNN module weights are not part of single contiguous chunk of memory. This means they need to be compacted at every call, possibly greately increasing memory usage. To compact weights again call flatten_parameters().
+  output, hidden = self.gru(output, hidden)
+/home/ubuntu/xinghao/cs101-xinghao/model.py:75: UserWarning: RNN module weights are not part of single contiguous chunk of memory. This means they need to be compacted at every call, possibly greately increasing memory usage. To compact weights again call flatten_parameters().
+  output, hidden = self.gru(output, hidden)
+Reading lines...
+Read 12771 sentence pairs
+Trimmed to 12771 sentence pairs
+Counting words...
+Counted words:
+diag1 1320
+diag2-4-from-other 1316
+load successfully
+> well i want you to see the house .
+= i m not going to change my mind dad .
+< i m not not my my m going to change ? <EOS>
+
+> what do you mean west s ?
+= i mean i killed him . isn t that what we all wanted ?
+< maybe he think . <EOS>
+
+> great .
+= well come over will you ? see the place ?
+< come you gonna be a big . <EOS>
+
+> what are you doing in my room ?
+= i didn t go in your room .
+< should i make a week . <EOS>
+
+> do i have to do everything around here ?
+= i hope so .
+< i hope so . <EOS>
+
+> what are you a cop ?
+= if i see you do it again i ll report it .
+< if i if i if i s looking at the s point i <EOS>
+
+> did you love your husband before you married ?
+= i did not know him before i married him . i loved his brother but then i married his brother too . he is dead now .
+< i but did you live but him but i didn t know what i i was all this time that . i m a bad girl . <EOS>
+
+> what seems to be the situation here ? you shit yourself or something ?
+= i wish .
+< i d t think . . . <EOS>
+
+> you sure you want to throw that card ?
+= sure .
+< sure . <EOS>
+
+> no more man . i m about to drop as it is .
+= see ya .
+< see you sir ? <EOS>
+
+input = i want to go with you !
+output = there ll be all right . <EOS>
+input = what are you doing ?
+output = she can t . <EOS>
+input = who knows ?
+output = that don t . . . . <EOS>
+input = relax .
+output = how the t you you you you ? <EOS>
+input = fuck you !
+output = i don t want . <EOS>
+input = do you love me ?
+output = yeah . i love me . <EOS>
+input = when can we marry ?
+output = sure . . <EOS>
